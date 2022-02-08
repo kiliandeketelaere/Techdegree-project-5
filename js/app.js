4a8f7191889c6cf951afaee47d891330fad1b89e
Kilin <deketelaerekilian@live.be>
2022-02-08 16:25:15 +0700

    Finished project

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+function search_gallery(){
+    let input = document.getElementById('search').value
+    input = input.toLowerCase();
+    let x = document.getElementsByTagName('li');
+
+    for (i = 0 ; i < x.length; i++) {
+        if(!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+    else {
+        x[i].style.display = "list-item";
+    }
+}
+};
+
+baguetteBox.run(".gallery");
+
+
+const search = new Filter('search', 'data-caption');
